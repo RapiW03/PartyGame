64f4f9d95dd3ec05c511f61aa7d9ee5ad012c807
RaphaelW03 <raphael.wolfsberger@gmx.at>
2023-01-25 09:04:50 +0100

    Server Setup
    Basic Setup Server
    Kein Main Code

---
+++ Server/app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const path = require('path');
+const routes = require('./routes/portfolio_management');
+const { errorHandler, notFound } = require('./middleware/errorHandler');
+const session = require('express-session');
+
+require('dotenv').config();
+
+const app = express();
+
+// let sessionStore = undefined;
+// const pgsession = require('connect-pg-simple')(session);
+
+// eslint-disable-next-line new-cap
+// sessionStore = new pgsession({
+//   pool: require('./db/index').pool,
+//   // Connection Pool
+//   tableName: 'user_sessions',
+// });
+
+// const sess = session({
+//   store: sessionStore,
+//   secret: 'awrecnireooahuo121345678765432345678',
+//   resave: false,
+//   saveUninitialized: false,
+//   cookie: {
+//     maxAge: 30 * 24 * 60 * 60 * 1000,
+//     httpOnly: true,
+//     secure: 'auto', // wenn https verwendet wird auf true setzen
+//   },
+// });
+
+let corsOption = {};
+const isDevelopment = true;
+
+app.use(helmet());
+if (isDevelopment) {
+  corsOption = {
+    origin: 'http://localhost:8080',
+    credentials: true,
+  };
+  const cors = require('cors');
+  app.use(cors(corsOption));
+}
+
+// app.use(sess);
+
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(express.json());
+app.use('/', persons);
+
+app.use(errorHandler);
+app.use(notFound);
+module.exports = app;
